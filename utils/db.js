8bca61c0345b85fcbcc0cc8f48e4b3bcf7506f31
St <aaronndas@gmail.com>
2024-09-30 23:56:30 +0200

    testing using alx envirnonment cause mine is failing

---
+++ utils/db.js
@@ -0,0 +1,30 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const uri = `mongodb://${host}:${port}/${database}`;
+    this.client = new MongoClient(uri);
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client.topology && this.client.topology.isConnected();
+  }
+
+  async nbUsers() {
+    const users = await this.client.db().collection('users').countDocuments();
+    return users;
+  }
+
+  async nbFiles() {
+    const files = await this.client.db().collection('files').countDocuments();
+    return files;
+  }
+}
+
+const dbClient = new DBClient();
+console.log(dbClient);
+module.exports = dbClient;
