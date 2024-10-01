f18535f9dae89feeccbdb0970db4668ae4c3de20
Kupakwashe Makunura <kupamakunura@gmail.com>
2024-10-01 20:23:46 +0200

    moving  envs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-const AppController = require('../controllers/AppController');
-const UsersController = require('../controllers/UsersController');
+const AppController = require("../controllers/AppController");
+const UsersController = require("../controllers/UsersController");
+const AuthController = require("../controllers/AuthController");
+const UserController = require("../controllers/");
 
 const injectRoutes = (app) => {
-  app.get('/status', AppController.getStatus);
-  app.get('/stats', AppController.getStats);
-  app.post('/users', UsersController.postNew);
+  app.get("/status", AppController.getStatus);
+  app.get("/stats", AppController.getStats);
+  app.post("/users", UsersController.postNew);
+  app.get("/connect", AuthController.getConnect);
+  app.get("/disconnect", AuthController.getDisconnect);
+  app.get("/users/me", UserController.getMe);
 };
 module.exports = injectRoutes;
